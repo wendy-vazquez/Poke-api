ab83c763558e91a174c7002036b9e17ca14fb9fb
Wendy-Vazquez <23308060610440@cetis61.edu.mx>
2025-11-12 09:25:46 -0700

    actu

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 
 app = Flask(__name__)
 app.secret_key = 'una_clave_secreta_muy_segura_y_larga_1234567890'
+API = 'https://pokeapi.co/api/v2/pokemon/'
 
 @app.route('/')
 def index():
@@ -14,7 +15,14 @@ def search():
     if not pokemon_name:
         flash('Por favor, ingresa un nombre de Pokémon.', 'error')
         return redirect(url_for('index'))
+   try:
+       resp = requests.get(f'{API}{pokemon_name}')
+       if resp.status_code == 200:
+           pokemon_data = resp.json()
+           return render_template('pokemon.html', pokemon=pokemon_data)
+       else:
+              flash('Pokémon no encontrado. Intenta con otro nombre.', 'error')
+              return redirect(url_for('index'))
     
-
 if __name__ == '__main__':
     app.run(debug=True)
